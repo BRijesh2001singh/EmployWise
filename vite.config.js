a0f15cc7fca52024659870eea55cd31b85fd7c2f
BrijeshSingh <brijesh1205singh@gmail.com>
2025-03-28 22:31:45 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss(),
+  ],
   server: {
     historyApiFallback: true, // Ensures React Router works in dev
   },
